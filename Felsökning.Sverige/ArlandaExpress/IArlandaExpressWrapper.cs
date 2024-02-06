ad93168ab67288c0b39a6b2076ee92ef609dea8a
felsokning <github@felsokning.se>
2024-02-06 12:13:28 +0000

    Adding Arlanda Express Interface for DI

---
+++ Felsökning.Sverige/ArlandaExpress/IArlandaExpressWrapper.cs
@@ -0,0 +1,15 @@
+﻿namespace Felsökning.Sverige.ArlandaExpress
+{
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="IArlandaExpressWrapper"/> interface.
+    /// </summary>
+    public interface IArlandaExpressWrapper
+    {
+        /// <summary>
+        ///     Obtains the list of departures for the given <see cref="City"/>.
+        /// </summary>
+        /// <param name="city">The <see cref="City"/> the departures are from.</param>
+        /// <returns>An awaitable <see cref="Task{TResult}"/> of <see cref="List{T}"/> of <see cref="Departure"/>.</returns>
+        Task<List<Departure>> GetDeparturesAsync(City city);
+    }
+}
