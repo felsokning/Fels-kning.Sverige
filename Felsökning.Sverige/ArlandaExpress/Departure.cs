4fce61932f5c486b7a58df2ecbcf5854ab2f2464
felsokning <github@felsokning.se>
2024-07-14 00:08:11 +0100

    Taking on Upstream Updates

--- Felsökning.Sverige/ArlandaExpress/Departure.cs
+++ Felsökning.Sverige/ArlandaExpress/Departure.cs
@@ -1,26 +1,50 @@
 ﻿namespace Felsökning.Sverige.ArlandaExpress
 {
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="Departure"/> class.
+    /// </summary>
     public class Departure
     {
+        /// <summary>
+        ///     Gets or sets the time left until departure.
+        /// </summary>
         [JsonPropertyName("timeLeft")]
         public int TimeLeft { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the date of the departure.
+        /// </summary>
         [JsonPropertyName("departureDate")]
-        public string DepartureDate { get; set; }
+        public string DepartureDate { get; set; } = string.Empty;
 
+        /// <summary>
+        ///     Gets or sets the time of the departure.
+        /// </summary>
         [JsonPropertyName("departureTime")]
-        public string DepartureTime { get; set; }
+        public string DepartureTime { get; set; } = string.Empty;
 
+        /// <summary>
+        ///     Gets or sets the day of the week of the departure.
+        /// </summary>
         [JsonPropertyName("departureDayOfWeek")]
-        public string DepartureDayOfWeek { get; set; }
+        public string DepartureDayOfWeek { get; set; } = string.Empty;
 
+        /// <summary>
+        ///     Gets or sets the date of the arrival.
+        /// </summary>
         [JsonPropertyName("arrivalDate")]
-        public string ArrivalDate { get; set; }
+        public string ArrivalDate { get; set; } = string.Empty;
 
+        /// <summary>
+        ///     Gets or sets the time of the arrival.
+        /// </summary>
         [JsonPropertyName("arrivalTime")]
-        public string ArrivalTime { get; set; }
+        public string ArrivalTime { get; set; } = string.Empty;
 
+        /// <summary>
+        ///     Gets or sets the day of the week of the arrival.
+        /// </summary>
         [JsonPropertyName("arrivalDayOfWeek")]
-        public string ArrivalDayOfWeek { get; set; }
+        public string ArrivalDayOfWeek { get; set; } = string.Empty;
     }
 }
--- Felsökning.Sverige/SMHI/Period.cs
+++ Felsökning.Sverige/SMHI/Period.cs
@@ -1,19 +1,37 @@
 ﻿namespace Felsökning.Sverige.SMHI
 {
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="Period"/> class.
+    /// </summary>
     public class Period
     {
+        /// <summary>
+        ///     Gets or sets the key of the period.
+        /// </summary>
         [JsonPropertyName("key")]
         public string Key { get; set; } = string.Empty;
 
+        /// <summary>
+        ///     Gets or sets the from of the period.
+        /// </summary>
         [JsonPropertyName("from")]
         public long From { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the to of the period.
+        /// </summary>
         [JsonPropertyName("to")]
         public long To { get; set; }
 
+        /// <summary>
+        ///     Gets or sets the summary of the period.
+        /// </summary>
         [JsonPropertyName("summary")]
         public string Summary { get; set; } = string.Empty;
 
+        /// <summary>
+        ///     Gets or sets the sampling of the period.
+        /// </summary>
         [JsonPropertyName("sampling")]
         public string Sampling { get; set; } = string.Empty;
     }
